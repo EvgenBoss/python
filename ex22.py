c0f37289ab3ec38a374d30912bdf492b308f6f8c
Evgen <Francesbean1999166@mail.ru>
2022-12-22 18:06:30 +0300

    факториал своими руками

---
+++ ex22.py
@@ -0,0 +1,11 @@
+x = int(input("Введите число\n"))
+lst_num = []
+i = 1
+for x in range(x):
+    i = i*(x+1)
+    lst_num.append(i)
+    print("[" , lst_num[x-1] , "*" , x+1 ,"=" , lst_num[x] , "]")
+print(lst_num)
+
+
+
