11074369c006268576a5472683a4fe2e11c4bfd2
Evgen <Francesbean1999166@mail.ru>
2022-12-28 00:13:28 +0300

    сумма нечетных элементов

---
+++ ex31.py
@@ -0,0 +1,10 @@
+x = list(map(int,input('Введите значения цен через пробел ').split()))
+z = len(x)
+e = 0
+k = 0
+for l in x:
+   k = k + x[e]
+   e = e + 2
+   if e > z-1:
+        break
+print(k)
